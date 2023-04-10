416318b57f356ab6798080f66d67149836d66691
Helderson Farias <helderson.farias@acct.global>
2023-04-10 13:47:19 -0300

    Ajustes para melhores práticas e convenções

--- Módulo 1/Javascript/Calculadora/script.js
+++ Módulo 1/Javascript/Calculadora/script.js
@@ -17,16 +17,16 @@ equal_button.addEventListener("click", equal);
 backspace_button.addEventListener("click", backspace);
 
 function click_number(e){
-    if (display.innerHTML == "0" || clear){
-        display.innerHTML = e.target.innerText;
-        clear = false;
-    } else{
-        display.innerHTML += e.target.innerText;
+    if (display.innerText != "0" && !clear){
+        display.innerText += e.target.innerText;
+        return;
     }
+    display.innerText = e.target.innerText;
+    clear = false;   
 }
 
 function click_operation(e){
-    display.innerHTML += " " + e.target.innerHTML + " ";
+    display.innerText += "&nbsp" + e.target.innerText + "&nbsp";
 }
 
 function equal(e){
@@ -36,8 +36,9 @@ function equal(e){
 
 function backspace(e){
     if (display.innerText.length > 1) {
-        display.innerText = display.innerText.slice(0, -1)
-    } else{
-        display.innerText = "0";
-    }
+        display.innerText = display.innerText.slice(0, -1);
+        return;
+    } 
+    
+    display.innerText = "0";
 }
